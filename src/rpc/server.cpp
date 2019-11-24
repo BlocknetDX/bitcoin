238a615990bde06a5bf935144c76c1facf14b7c3
Hanni Abu <hanniabu@gmail.com>
2019-11-24 05:02:23 -0500

    Update help messages Copy over magic53's code to properly capitalize XBridge and XRouter

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -211,7 +211,13 @@ std::string CRPCTable::help(const std::string& strCommand, const JSONRPCRequest&
                     if (!category.empty())
                         strRet += "\n";
                     category = pcmd->category;
-                    strRet += "== " + Capitalize(category) + " ==\n";
+                    if (category == "xbridge" || category == "xrouter") { // uppercase first two letters
+                        string firstLetter = category.substr(0, 2);
+                        boost::to_upper(firstLetter);
+                        strRet += "== " + firstLetter + category.substr(2) + " ==\n";
+                    } else {
+                        strRet += "== " + Capitalize(category) + " ==\n";
+                    }
                 }
             }
             strRet += strHelp + "\n";
